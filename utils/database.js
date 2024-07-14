64ea05f3c5fe9c539971c974128289cf8f5fba27
mayankchadha28 <mayankchadha1998@gmail.com>
2024-07-14 19:49:42 -0400

    login issue: db

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: 'prompt-app',
-            useNewUrlParser: true,
-            useUnifiedTopology: true
         })
 
         isConnected = true
